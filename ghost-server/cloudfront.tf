b9407d52c9e8912bebb7f694c87d8ac7ede603fc
Cory Finger <cory.j.finger@gmail.com>
2018-01-22 00:40:17 -0800

    Optimized caching rules

--- ghost-server/cloudfront.tf
+++ ghost-server/cloudfront.tf
@@ -43,6 +43,60 @@ resource "aws_cloudfront_distribution" "ghost-blog" {
     max_ttl                = 86400
   }
 
+  cache_behavior {
+    path_pattern      = "assets/*"
+    allowed_methods   = ["GET", "HEAD"]
+    cached_methods    = ["GET", "HEAD"]
+    target_origin_id  = "${var.name}-origin"
+    compress          = true
+
+    forwarded_values {
+      query_string  = true
+      cookies { forward = "none" }
+    }
+
+    viewer_protocol_policy = "redirect-to-https"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+  }
+
+  cache_behavior {
+    path_pattern      = "content/*"
+    allowed_methods   = ["GET", "HEAD"]
+    cached_methods    = ["GET", "HEAD"]
+    target_origin_id  = "${var.name}-origin"
+    compress          = true
+
+    forwarded_values {
+      query_string  = true
+      cookies { forward = "none" }
+    }
+
+    viewer_protocol_policy = "redirect-to-https"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+  }
+
+  cache_behavior {
+    path_pattern      = "public/*"
+    allowed_methods   = ["GET", "HEAD"]
+    cached_methods    = ["GET", "HEAD"]
+    target_origin_id  = "${var.name}-origin"
+    compress          = true
+
+    forwarded_values {
+      query_string  = true
+      cookies { forward = "none" }
+    }
+
+    viewer_protocol_policy = "redirect-to-https"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+  }
+
   tags {
     Environment = "${var.name}-production"
   }
